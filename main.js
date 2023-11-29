5637c08b8905ffc6da6757ec3c94c537d412b5d1
Diego Steiner <diego.steiner@u041.ch>
2023-11-29 10:43:46 +0100

    add redis code

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const fastify = require('fastify')({ logger: true });
-const redis = require('fastify-redis');
 
-// fastify.register(require('@fastify/redis'), { url: process.env.REDIS_URL })
+fastify.register(require('@fastify/redis'), { url: process.env.REDIS_URL })
 fastify.register(require('@fastify/formbody'))
 fastify.register(require("@fastify/view"), {
 	engine: {
@@ -14,19 +13,21 @@ let posts = [
 	{ id: 2, text: 'Another post here.' },
 ];
 
-function getPosts() {
-	return posts
-	// const postsFromRedis = await fastify.redis.get('posts');
-	// return postsFromRedis ? JSON.parse(postsFromRedis) : posts;
+async function getPosts() {
+	// return posts
+	const postsFromRedis = await fastify.redis.get('posts');
+	return postsFromRedis ? JSON.parse(postsFromRedis) : posts;
 }
 
-fastify.get("/", (request, response) => {
-	response.view("/index.ejs", { posts: getPosts() });
+fastify.get("/", async (request, response) => {
+	const posts = await getPosts()
+	return response.view("/index.ejs", { posts });
 });
 
 fastify.get('/posts', async (request, response) => {
 	try {
-		response.send({ posts: getPosts() });
+		const posts = await getPosts()
+		response.send({ posts });
 	} catch (error) {
 		response.status(500).send({ error: 'Internal Server Error' });
 	}
@@ -34,11 +35,11 @@ fastify.get('/posts', async (request, response) => {
 
 fastify.post('/posts', async (request, response) => {
 	try {
-		const posts = getPosts()
+		const posts = await getPosts()
 		const newPost = { id: posts.length + 1, text: request.body.text };
 		posts.push(newPost);
 
-		// await fastify.redis.set('posts', JSON.stringify(posts));
+		await fastify.redis.set('posts', JSON.stringify(posts));
 		response.redirect('/')
 
 	} catch (error) {
